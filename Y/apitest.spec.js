f14a7981d71df2708af763b06ff71bbdd8c935ff
Mar-Sieraszewska <martyna.sieraszewska@wp.pl>
2025-06-30 20:32:39 +0200

    xxx

--- Y/apitest.spec.js
+++ Y/apitest.spec.js
@@ -7,6 +7,7 @@ test('get', async ({ request }) => {
     const response = await request.get('api/productsList');
     expect(response.ok()).toBeTruthy();
     const data = await response.json();
+    console.log(data);
     expect(data).toHaveProperty('products');
     expect(data.products.length).toBeGreaterThan(0);
-)};
+});
